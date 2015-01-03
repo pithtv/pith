3c50756c0fe7c24c7380850a1067a2d58024de07
Christoph Cantillon <evinyatar@evinyatar.be>
2015-01-03 14:54:21 +0100

    fix Windows path issue

--- plugins/files/fanart.js
+++ plugins/files/fanart.js
@@ -9,9 +9,7 @@ module.exports = {
         var tbnFile = $path.resolve(filepath, "fanart.jpg");
         fs.exists(tbnFile, function(exists) {
             if(exists) {
-                var path = channel.rootDir;
-                var matchRootDir = new RegExp("^" + path);
-                var itemPath = tbnFile.replace(matchRootDir, '').split(path.sep).map(encodeURIComponent).join("/");
+                var itemPath = $path.relative(channel.rootDir, item.id).split($path.sep).map(encodeURIComponent).join("/");
                 item.fanart = channel.pith.rootPath + "/stream/" + itemPath;
             }
             cb();
