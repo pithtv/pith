546a31de76e274eed3540750ce141335cf5af909
Christoph Cantillon <christoph.cantillon@unifly.aero>
2016-06-14 21:48:14 +0200

    inline episode guide

---
+++ webui/js/controller.tvdetails.js
@@ -0,0 +1,37 @@
+angular
+    .module("pith.tvDetailsModule", ["pith.restApi"])
+    .controller("TvDetailsController", ["$scope", "$pithRest", function($scope, $pithRest) {
+
+        var channel = $pithRest.channel($scope.channelId);
+        var self = this;
+
+        $scope.selectedSeason = null;
+
+        function fetchSeasons(show) {
+            if(show) {
+                channel.list(show.id).then(function(seasons) {
+                    $scope.seasons = seasons.data;
+                    if(!$scope.selectedSeason || $scope.seasons.indexOf($scope.selectedSeason) == -1) {
+                        $scope.selectedSeason = $scope.seasons[$scope.seasons.length - 1];
+                    }
+                });
+            }
+        }
+
+        function fetchEpisodes(season) {
+            if(season) {
+                channel.list(season.id).then(function(episodes) {
+                    $scope.episodes = episodes.data;
+                })
+            }
+        }
+
+        $scope.selectSeason = function(season) {
+            $scope.selectedSeason = season;
+            $scope.episodes = null;
+        }
+
+        $scope.$watch('$expandedItem', fetchSeasons);
+        $scope.$watch('selectedSeason', fetchEpisodes);
+
+}]);
