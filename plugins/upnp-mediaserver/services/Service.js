bcbe4519bd2626309ba5ef1089df15645c8f9465
evinyatar <christoph.cantillon@gmail.com>
2018-12-09 09:36:40 +0100

    add accreditation

--- plugins/upnp-mediaserver/services/Service.js
+++ plugins/upnp-mediaserver/services/Service.js
@@ -1,3 +1,18 @@
+/**
+ * Based on node-upnp-device:
+ * Copyright (c) 2011 Jacob Rask, <http://jacobrask.net>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
+
 const {DeviceControlProtocol} = require("../DeviceControlProtocol");
 const { processors, Parser } = require('xml2js');
 const gen_uuid = require('node-uuid');
