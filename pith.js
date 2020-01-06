18f859ae2e010fc34ac40cc6f7dc2d425d478cf0
evinyatar <christoph.cantillon@gmail.com>
2020-01-06 20:33:18 +0100

    promisify playback control code

--- pith.js
+++ pith.js
@@ -9,7 +9,7 @@ const route = express.Router();
 let sequence = 0;
 
 function newId() {
-    return "id"+(sequence++);
+    return "id" + (sequence++);
 }
 
 class Pith extends EventEmitter {
@@ -33,7 +33,7 @@ class Pith extends EventEmitter {
         let channelInstance = this.channelInstances[channelId];
         if (channelInstance === undefined) {
             const channel = this.channelMap[channelId];
-            if(channel !== undefined) {
+            if (channel !== undefined) {
                 channelInstance = this.channelInstances[channelId] = channel.init({pith: this});
                 channelInstance.id = channelId;
             }
@@ -45,15 +45,16 @@ class Pith extends EventEmitter {
         this.channels.push(channel);
         this.channelMap[channel.id] = channel;
         this.channels.sort(function (a, b) {
-            if (a.sequence === b.sequence) { return 0; }
-            else if(a.sequence < b.sequence) return -1;
+            if (a.sequence === b.sequence) {
+                return 0;
+            } else if (a.sequence < b.sequence) return -1;
             else return 1;
         });
         this.emit("channelRegistered", {channel: channel});
     }
 
     registerPlayer(player) {
-        if(!player.id) player.id = newId();
+        if (!player.id) player.id = newId();
         this.players.push(player);
         this.playerMap[player.id] = player;
         this.emit("playerregistered", {player: player});
@@ -73,7 +74,7 @@ class Pith extends EventEmitter {
     updatePlayerStates() {
         const newTs = new Date().getTime();
         this.players.forEach((e) => {
-            if(e.status.state && e.status.state.playing) {
+            if (e.status.state && e.status.state.playing) {
                 const delta = (newTs - e.status.serverTimestamp) / 1000;
                 e.status.position.time += delta;
                 e.status.serverTimestamp = newTs;
@@ -97,7 +98,7 @@ class Pith extends EventEmitter {
 
     putPlayState(channelId, itemId, state, cb) {
         const channelInstance = this.getChannelInstance(channelId);
-        if(channelInstance) {
+        if (channelInstance) {
             if (state.status !== undefined || state.duration > 600) {
                 if (!state.status) {
                     if (state.time > Math.max(state.duration - 300, state.duration * 11 / 12)) {
@@ -116,31 +117,23 @@ class Pith extends EventEmitter {
 
     loadMedia(channelId, itemId, playerId, cb, opts) {
         let player = this.playerMap[playerId];
-        if(!player) {
+        if (!player) {
             cb(new Error(`Unknown player ${playerId}`));
             return;
         }
         let channel = this.getChannelInstance(channelId);
-        channel.getItem(itemId).then((item) => {
-            player.load(channel, item, function(err) {
-                if(err) {
-                    cb(err);
-                } else {
-                    player.play(function(err) {
-                        cb(err);
-                    }, opts && opts.time);
-                }
-            });
-        }).catch(err => cb(err));
+        channel.getItem(itemId).then(
+            item => player.load(channel, item)
+        ).then(
+            () => player.play(opts && opts.time)
+        ).then(
+            () => cb(false)
+        ).catch(err => cb(err));
     }
 
-    controlPlayback(playerId, command, query, cb) {
+    controlPlayback(playerId, command, query) {
         const player = this.playerMap[playerId];
-        if(typeof query === 'function') {
-            cb = query;
-            query = undefined;
-        }
-        player[command](cb, query);
+        return player[command](query);
     }
 
     load() {
@@ -153,7 +146,7 @@ class Pith extends EventEmitter {
     }
 
     settings(settings) {
-        if(arguments.length === 0) {
+        if (arguments.length === 0) {
             return global.settings;
         } else {
             global.storeSettings(settings);
--- plugins/upnp-mediarenderer/plugin.js
+++ plugins/upnp-mediarenderer/plugin.js
@@ -3,10 +3,11 @@ const xml2js = require("xml2js").parseString;
 const {EventEmitter} = require("events");
 const entities = require("entities");
 const Device = require("upnp-client-minimal");
-const {formatTime,formatMsDuration} = require('../../lib/upnp');
+const {formatTime, formatMsDuration} = require('../../lib/upnp');
 const Global = require("../../lib/global")();
 const client = ssdp({unicastHost: Global.bindAddress});
 const logger = require('log4js').getLogger('pith.plugin.upnp-mediarenderer');
+const {wrap} = require("../../lib/async");
 
 const iconTypePreference = [
     'image/jpeg',
@@ -18,19 +19,19 @@ const iconTypePreference = [
 const players = {};
 
 function parseTime(time) {
-    if(!time) {
+    if (!time) {
         return undefined;
     } else {
-        return time.split(":").reduce(function(a,b) {
-            return a*60 + parseInt(b,10);
+        return time.split(":").reduce(function (a, b) {
+            return a * 60 + parseInt(b, 10);
         }, 0);
     }
 }
 
 function _(t) {
-    if(!t) return t;
+    if (!t) return t;
     const x = t._ || t;
-    if(typeof x === 'string') {
+    if (typeof x === 'string') {
         return x;
     } else {
         return undefined;
@@ -73,183 +74,178 @@ class MediaRenderer extends EventEmitter {
         this.startWatching();
     }
 
-    load(channel, item, cb) {
-        const renderer = this;
+    async load(channel, item) {
+        let stream = await channel.getStream(item, {fingerprint: [Global.persistentUuid('instance'), channel.id, item.id].join(':')});
+        const mediaUrl = stream.url;
+        logger.debug(`Loading ${mediaUrl}`);
 
-        channel.getStream(item, {fingerprint: [Global.persistentUuid('instance'), channel.id, item.id].join(':')}).then(stream => {
-            const mediaUrl = stream.url;
-            logger.debug(`Loading ${mediaUrl}`);
+        if (this.status.actions.stop) {
+            await this.stop();
+        }
 
-            function doLoad() {
-                const type = stream.mimetype.split('/')[0];
+        const type = stream.mimetype.split('/')[0];
 
-                renderer._avTransport.SetAVTransportURI({
-                    InstanceID: 0,
-                    CurrentURI: mediaUrl,
-                    CurrentURIMetaData:
-                        entities.encodeXML(
-                            `<DIDL-Lite xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:pith="http://github.com/evinyatar/pith/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/"><item id="${entities.encodeXML(item.id)}" parentID="0" restricted="1"><dc:title>${entities.encodeXML(item.title)}</dc:title><upnp:class>object.item.${type}Item</upnp:class><res duration="${formatMsDuration(stream.duration)}" protocolInfo="http-get:*:${stream.mimetype}:DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=01500000000000000000000000000000">${mediaUrl}</res><pith:itemId>${entities.encodeXML(item.id)}</pith:itemId><pith:channelId>${entities.encodeXML(channel.id)}</pith:channelId></item></DIDL-Lite>
-`)
-                }, cb);
-            }
-
-            if(renderer.status.actions.stop) {
-                renderer.stop(function(err) {
-                    doLoad();
-                });
-            } else {
-                doLoad();
-            }
-        }).catch(cb);
-    }
-
-    play(cb, time) {
-        const renderer = this;
-        this._avTransport.Play({
-            InstanceID: 0,
-            Speed: 1
-        }, err => {
-            if(err) {
-                logger.error("Error starting playback on UPnP device", err);
-                if(cb) cb(err);
-                return;
-            }
-
-            if(time) {
-                const timeout = new Date().getTime() + 3000;
-
-                const waitForSeek = function(status) {
-                    if(status.actions.seek) {
-                        renderer.seek(function(err) {
-                            if(err) {
-                                renderer.once('statechange', waitForSeek);
-                            } else {
-                                cb && cb();
-                            }
-                        }, { time: time });
-                    } else if(new Date().getTime() < timeout) {
-                        renderer.once('statechange', waitForSeek);
-                    } else if(cb) {
-                        cb("Seeking not available");
-                    }
-                };
-
-                renderer.once('statechange', waitForSeek);
-            } else {
-                cb();
-            }
+        await wrap(cb => {
+            this._avTransport.SetAVTransportURI({
+                InstanceID: 0,
+                CurrentURI: mediaUrl,
+                CurrentURIMetaData:
+                    entities.encodeXML(
+                        `<DIDL-Lite xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/" xmlns:pith="http://github.com/evinyatar/pith/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/" xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/"><item id="${entities.encodeXML(item.id)}" parentID="0" restricted="1"><dc:title>${entities.encodeXML(item.title)}</dc:title><upnp:class>object.item.${type}Item</upnp:class><res duration="${formatMsDuration(stream.duration)}" protocolInfo="http-get:*:${stream.mimetype}:DLNA.ORG_OP=01;DLNA.ORG_CI=0;DLNA.ORG_FLAGS=01500000000000000000000000000000">${mediaUrl}</res><pith:itemId>${entities.encodeXML(item.id)}</pith:itemId><pith:channelId>${entities.encodeXML(channel.id)}</pith:channelId></item></DIDL-Lite>`)
+            }, cb)
         });
     }
 
-    stop(cb) {
-        this._avTransport.Stop({
-            InstanceID: 0
-        }, cb);
+    async play(time) {
+        const renderer = this;
+        try {
+            await wrap(cb => this._avTransport.Play({
+                InstanceID: 0,
+                Speed: 1
+            }, cb));
+
+            if (time) {
+                const timeout = new Date().getTime() + 3000;
+                return new Promise((resolve, reject) => {
+                    const waitForSeek = async (status) => {
+                        if (status.actions.seek) {
+                            try {
+                                await this.seek({time: time});
+                                this.removeListener('statechange', waitForSeek);
+                            } catch (err) {
+                                logger.error(`Seeking failed`, err);
+                                this.removeListener('statechange', waitForSeek);
+                            }
+                        } else if (new Date().getTime() > timeout) {
+                            this.removeListener('statechange', waitForSeek);
+                            logger.error('Seeking not available');
+                        }
+                    };
+
+                    renderer.on('statechange', waitForSeek);
+                });
+            }
+        } catch (err) {
+            logger.error("Error starting playback on UPnP device", err);
+            throw err;
+        }
     }
 
-    pause(cb) {
-        this._avTransport.Pause({
+    stop() {
+        return wrap(cb => this._avTransport.Stop({
             InstanceID: 0
-        }, cb);
+        }, cb));
     }
 
-    seek(cb, query) {
+    pause() {
+        return wrap(cb => this._avTransport.Pause({
+            InstanceID: 0
+        }, cb));
+    }
+
+    seek(query) {
         const time = formatTime(query.time);
-        this._avTransport.Seek({
+        return wrap(cb => this._avTransport.Seek({
             InstanceID: 0,
             Unit: "REL_TIME",
             Target: time
-        }, cb);
+        }, cb));
     }
 
-    getPositionInfo(cb) {
-        this._avTransport.GetPositionInfo({InstanceID: 0}, (err, positionInfo) => {
-            if(err) {
-                cb(err); return;
+    async getPositionInfo() {
+        const positionInfo = await wrap(cb => this._avTransport.GetPositionInfo({InstanceID: 0}, cb));
+
+        const pos = {
+            track: positionInfo.Track,
+            AbsCount: positionInfo.AbsCount,
+            RelCount: positionInfo.RelCount,
+            AbsTime: parseTime(_(positionInfo.AbsTime)),
+            time: parseTime(_(positionInfo.RelTime)),
+            uri: positionInfo.TrackURI,
+            duration: parseTime(_(positionInfo.TrackDuration))
+        };
+
+        if (positionInfo.TrackMetaData && typeof positionInfo.TrackMetaData === 'string') {
+            const meta = await wrap(cb => xml2js(positionInfo.TrackMetaData, cb));
+            const didlLite = meta['DIDL-Lite'].item[0];
+            pos.fingerprint = decodeURIComponent(didlLite.$.id.match(Global.persistentUuid('instance') + "[^/]*"));
+            for (let x of Object.keys(didlLite)) {
+                const val = didlLite[x][0];
+                switch (x) {
+                    case 'dc:title':
+                        pos.title = val;
+                        break;
+                    case 'upnp:genre':
+                        pos.genre = val;
+                        break;
+                    case 'pith:itemId':
+                        pos.itemId = val;
+                        break;
+                    case 'pith:channelId':
+                        pos.channelId = val;
+                        break;
+                }
             }
+            return pos;
+        }
+    }
 
-            const pos = {
-                track: positionInfo.Track,
-                AbsCount: positionInfo.AbsCount,
-                RelCount: positionInfo.RelCount,
-                AbsTime: parseTime(_(positionInfo.AbsTime)),
-                time: parseTime(_(positionInfo.RelTime)),
-                uri: positionInfo.TrackURI,
-                duration: parseTime(_(positionInfo.TrackDuration))
-            };
-
-            if(positionInfo.TrackMetaData && typeof positionInfo.TrackMetaData === 'string') {
-                xml2js(positionInfo.TrackMetaData, (err, meta) => {
-                    if(!err) {
-                        const didlLite = meta['DIDL-Lite'].item[0];
-                        pos.fingerprint = decodeURIComponent(didlLite.$.id.match(Global.persistentUuid('instance') + "[^/]*"));
-                        for(let x in didlLite) {
-                            const val = didlLite[x][0];
-                            switch(x) {
-                                case 'dc:title': pos.title = val; break;
-                                case 'upnp:genre': pos.genre = val; break;
-                                case 'pith:itemId': pos.itemId = val; break;
-                                case 'pith:channelId': pos.channelId = val; break;
-                            }
-                        }
-                        cb(null, pos);
-                    } else {
-                        cb(err);
-                    }
+    async updatePositionInfo() {
+        const positionInfo = await this.getPositionInfo();
+        if (positionInfo) {
+            let channelId = positionInfo.channelId, itemId = positionInfo.itemId;
+            if (!channelId || !itemId) {
+                const fingerprint = positionInfo.fingerprint;
+                const parts = fingerprint.match(/^[^:]*:([^:]*):(.*)$/);
+                if (parts) {
+                    [, channelId, itemId] = parts;
+                }
+            }
+            if (channelId && itemId) {
+                this._opts.pith.putPlayState(channelId, itemId, {
+                    time: positionInfo.time,
+                    duration: positionInfo.duration
                 });
             }
-        });
-    }
 
-    updatePositionInfo(cb) {
-        this.getPositionInfo((err, positionInfo) => {
-            if(!err) {
-                let channelId = positionInfo.channelId, itemId = positionInfo.itemId;
-                if(!channelId || !itemId) {
-                    const fingerprint = positionInfo.fingerprint;
-                    const parts = fingerprint.match(/^[^:]*:([^:]*):(.*)$/);
-                    if(parts) {
-                        [,channelId,itemId] = parts;
-                    }
-                }
-                if(channelId && itemId) {
-                    this._opts.pith.putPlayState(channelId, itemId, {time: positionInfo.time, duration: positionInfo.duration});
-                }
-
-                this.status.position = positionInfo;
-                this.emit('statechange', this.status);
-            }
-            if(cb) {
-                cb(err);
-            }
-        });
+            this.status.position = positionInfo;
+            this.emit('statechange', this.status);
+        }
     }
 
     startWatching() {
         this._avTransport.on('LastChange', (changeEvent) => {
             xml2js(changeEvent, (err, body) => {
-                if(!body) {
+                if (!body) {
                     logger.error("Empty body in LastChange event");
-                } else if(err) {
+                } else if (err) {
                     logger.error("Error in LastChange event", err);
                 } else {
                     const didl = body.Event.InstanceID[0];
 
-                    if(didl.CurrentTransportActions) {
+                    if (didl.CurrentTransportActions) {
                         const actions = didl.CurrentTransportActions[0].$.val.match(/(([^,\\]|\\\\|\\,|\\)+)/g);
                         this.status.actions = {};
-                        if(actions) actions.forEach((state) => {
+                        if (actions) actions.forEach((state) => {
                             this.status.actions[state.toLowerCase()] = true;
                         });
                     }
-                    if(didl.TransportState) {
+                    if (didl.TransportState) {
                         let state = didl.TransportState[0];
                         let statusFlags;
-                        switch(state.$.val) {
-                                case 'PAUSED_PLAYBACK': statusFlags = {paused: true}; break;
-                                case 'PLAYING': statusFlags = {playing: true}; break;
-                                case 'STOPPED': statusFlags = {stopped: true}; break;
-                                case 'TRANSITIONING': statusFlags = {transitioning: true}; break;
+                        switch (state.$.val) {
+                            case 'PAUSED_PLAYBACK':
+                                statusFlags = {paused: true};
+                                break;
+                            case 'PLAYING':
+                                statusFlags = {playing: true};
+                                break;
+                            case 'STOPPED':
+                                statusFlags = {stopped: true};
+                                break;
+                            case 'TRANSITIONING':
+                                statusFlags = {transitioning: true};
+                                break;
                         }
                         this.status.state = statusFlags;
                     }
@@ -267,7 +263,7 @@ class MediaRenderer extends EventEmitter {
 
     offline() {
         logger.info(`Device went offline: ${this.friendlyName}`);
-        if(this.positionTimer) {
+        if (this.positionTimer) {
             clearInterval(this.positionTimer);
             this.positionTimer = null;
         }
@@ -279,12 +275,11 @@ module.exports = {
         const plugin = this;
 
         function handlePresence(data, rinfo) {
-            if(!players[data.USN]) {
-                players[data.USN] = {}; // placeholder so we don't make them twice in case the alive is triggered before createMediaRenderer finishes
-                plugin.handlePlayerAppearance(data, rinfo, opts, function(err, renderer) {
-                    if(err) {
+            if (!players[data.USN]) {
+                plugin.handlePlayerAppearance(data, rinfo, opts, function (err, renderer) {
+                    if (err) {
                         logger.log("Error in handlePlayerAppearance", err);
-                    } else {
+                    } else if(!(data.USN in players)) {
                         players[data.USN] = renderer;
                         opts.pith.registerPlayer(renderer);
                     }
@@ -293,7 +288,7 @@ module.exports = {
         }
 
         function handleDisappearance(data) {
-            if(players[data.USN]) {
+            if (data.USN in players) {
                 players[data.USN].offline();
                 opts.pith.unregisterPlayer(players[data.USN]);
                 players[data.USN] = undefined;
@@ -309,11 +304,11 @@ module.exports = {
 
     handlePlayerAppearance(headers, rinfo, opts, cb) {
         const plugin = this;
-        Device({descriptorUrl: headers.LOCATION}, function(err, device) {
-            if(err) {
+        Device({descriptorUrl: headers.LOCATION}, function (err, device) {
+            if (err) {
                 cb(err);
             } else {
-                plugin.createRenderer(device, opts, function(err, renderer) {
+                plugin.createRenderer(device, opts, function (err, renderer) {
                     cb(false, renderer);
                 });
             }
