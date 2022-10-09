f9448221adf9eef49deb253beced5967c0e62374
ccantill <christoph.cantillon@unifly.aero>
2022-10-09 18:20:25 +0200

    fix refreshing in web browser resulting in 404

--- packages/server/src/plugins/webui/plugin.ts
+++ packages/server/src/plugins/webui/plugin.ts
@@ -8,7 +8,7 @@ import webUiPath from "@pithmediaserver/pith-webui";
 export default class WebUIPlugin implements PithPlugin {
     async init({pith}: {pith: Pith}) {
         pith.express.use('/webui', express.static(webUiPath, {fallthrough: true}));
-        pith.express.all('/webui/*', (req, res, next) => res.sendFile('index.html', { root: path.resolve(__dirname, '..', '..', '..', 'webui', 'dist') }))
+        pith.express.all('/webui/*', (req, res, next) => res.sendFile('index.html', { root: webUiPath }))
         pith.express.use(/\/$/, (req, res, next) => {
             res.redirect('/webui/');
         });
